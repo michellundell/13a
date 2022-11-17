19fa8b2a57855a6655aec09a209cfd0b68cd328a
Michel Lundell <michel@lundell.net>
2022-11-17 10:00:49 +0100

    Update sample.c

--- sample.c
+++ sample.c
@@ -2,35 +2,28 @@
 * This example is copied from
 * https://www.thegeekstuff.com/2013/06/buffer-overflow/
 */
-
 #include <stdio.h>
 #include <string.h>
 
-
 int main(int argc, char **argv)
 {
-    char buff[15];
+    char buff[15],otherbuf[10];
     int pass = 0;
 
     printf("\n Enter the password : \n");
-    gets(buff);
+    scanf("%s",buff);
 
-    if(strcmp(buff, "thesecretpassword"))
-    {
+    if(strcmp(buff, "thesecretpass")) {
         printf ("\n Wrong Password \n");
-    }
-    else
-    {
+    } else {
         printf ("\n Correct Password \n");
         pass = 1;
     }
 
-    if(pass)
-    {
+    if(pass) {
        /* Now Give root or admin rights to user*/
         printf ("\n Root privileges given to the user \n");
     }
 
     return(0);
 }
-
